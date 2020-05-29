ea1eca3feb95b294e4ec344c5b49c2cef79c3677
hhhrrrttt222111 <hemanthrt18@gmail.com>
2020-05-29 11:10:02 +0530

    wiki

---
+++ SCRAPING/Wikipedia_search.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+input_search = input('Search Wikipedia : ')
+
+
+driver = webdriver.Chrome(executable_path=r"D:\Softwares\chromedriver_win32\chromedriver.exe")
+driver.get("https://www.wikipedia.org/")
+
+search = driver.find_element_by_name('search')
+search.send_keys(input_search)
+search.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+url = driver.current_url
+
+res = requests.get(url)
+res.raise_for_status()
+
+wiki = BeautifulSoup(res.text, 'html.parser')
+
+for i in wiki.select('p'):
+    print(i.getText(), end='')
